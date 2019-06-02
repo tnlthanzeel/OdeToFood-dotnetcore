4a566b8ee13f3af3f94f2c18464f17a48eb02d68
Thanzeel <tnlthanzeel@gmail.com>
2019-06-02 12:59:48 +0530

    Add project files.

---
+++ OdeToFood/Pages/Restuarants/List.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood.Pages.Restuarants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration configuration;
+
+        public ListModel(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = configuration["Message"];
+        }
+    }
+}
